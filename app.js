8086dd754e0a810a07b8543044b9b32d54fdba0d
zmunro <zacharymunro2@gmail.com>
2019-02-11 18:16:46 -0500

    put banned IPs in table

--- app.js
+++ app.js
@@ -11,38 +11,15 @@ var baseUri   = process.env.PASSENGER_BASE_URI || '/';
 var router = express.Router();
 
 router.get("/", function(request, response){
-  exec('/usr/local/bin/inventory', function(error, stdout, stderr){
+  exec('/usr/local/bin/listbannedipxfer', function(error, stdout, stderr){
     var lines = stdout.toString().replace(/  +/g, ' ').split('\n');
-
-    var dict = {};
-    
-    for(i = 1; i < lines.length - 4; i++) {
-        node = lines[i].replace(/\s+/g, ' ').split(' ')[0];
-        node = node.slice(0,-2);
-        
-        if (node[node.length - 1] == "0") {
-            node = node.slice(0,-1);
-        }
-
-        if (dict[node] === null|| dict[node] === undefined) {
-            dict[node] = 1;
-        } else {
-            dict[node]++;
-        }
-    }
-    
-    var vals = Object.keys(dict).map(function(key) {
-        return dict[key];
-    });
     
     response.render('index', {
       baseUri: baseUri,
       date: new Date(),
       output: lines,
-      title: "Inventory",
+      title: "Banned IPs",
       error: stderr,
-      nodes: Object.keys(dict),
-      counts: vals
       // uncomment if you want to reload:
       // reload: 15
     });
